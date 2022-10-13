2f2b999fb40ed7cf1f777d6e0a854090e24f81c5
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-13 14:49:45 -0400

    adds GET and POST for edit subdco

--- controllers/feeding.js
+++ controllers/feeding.js
@@ -2,41 +2,13 @@
 // Import Dependencies
 const express = require('express')
 const Baby = require('../models/baby')
-const Feeding = require('../models/feeding')
+
 
 
 // Create Router
 const router = express.Router()
 
 // Routes
-
-// // GET for new feeding
-// // renders the form to create a feeding
-// router.get('/new', (req, res) => {
-//     const username = req.session.username
-//     const loggedIn = req.session.loggedIn
-//     const userId = req.session.userId
-//     res.render('feedings/new', { username, loggedIn, userId })
-// })
-
-// // GET - Show one feeding
-// router.get('/:feedingId', (req, res) => {
-// 	const feedingId = req.params.feedingId
-//     .populate('feedings.baby', 'firstName')
-// 	Feeding.findById(feedingId)
-// 		.then(feeding => {
-// 			const username = req.session.username
-// 			const loggedIn = req.session.loggedIn
-// 			const userId = req.session.userId
-// 			res.render('feedings/show', { feeding, username, loggedIn, userId })
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-
-
 // POST
 router.post('/:babyId', (req, res) => {
     const babyId = req.params.babyId
@@ -55,6 +27,49 @@ router.post('/:babyId', (req, res) => {
         })
         .then(baby => {
             res.redirect(`/babies/${baby.id}`)
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
+})
+
+// GET - Show edit page
+router.get('/:babyId/:feedingId/edit', (req, res) => {
+    const username = req.session.username
+	const loggedIn = req.session.loggedIn
+	const userId = req.session.userId
+    const babyId = req.params.babyId 
+    const feedingId = req.params.feedingId
+    Baby.findById(babyId)
+        .then(baby => {
+            const theFeeding = baby.feedings.id(feedingId)
+            res.render('feedings/edit', { baby, theFeeding, username, loggedIn, userId })
+        })
+        .catch(error => {
+            res.redirect(`/error?error=${error}`)
+        })
+})
+
+// PUT - Edit subdoc
+router.put('/:babyId/:feedingId', (req, res) => {
+    const babyId = req.params.babyId 
+    const feedingId = req.params.feedingId
+    req.body.bottle = req.body.bottle === 'on' ? true : false
+    req.body.breast = req.body.breast === 'on' ? true : false
+    Baby.findById(babyId)
+        .then(baby => {
+            const theFeeding = baby.feedings.id(feedingId)
+            if (req.session.loggedIn) {
+                if (theFeeding.parent == req.session.userId) {
+                    theFeeding.set(req.body)
+                    baby.save()
+                    res.redirect(`/babies/${baby.id}`)
+                } else {
+                    const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+                }
+            } else {
+                const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+            }
         })
         .catch(err => res.redirect(`/error?error=${err}`))
 })
