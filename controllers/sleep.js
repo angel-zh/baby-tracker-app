8dd15bfcfab2784fae89a0ffb8654b18bc7b045f
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-13 15:33:36 -0400

    adds GET and PUT routes for subdoc

--- controllers/sleep.js
+++ controllers/sleep.js
@@ -1,7 +1,7 @@
 // Import Dependencies
 const express = require('express')
 const Baby = require('../models/baby')
-const Sleep = require('../models/sleep')
+
 
 
 // Create Router
@@ -11,8 +11,7 @@ const router = express.Router()
 // POST
 router.post('/:babyId', (req, res) => {
     const babyId = req.params.babyId
-    // req.body.bottle = req.body.bottle === 'on' ? true : false
-    // req.body.breast = req.body.breast === 'on' ? true : false
+
     if (req.session.loggedIn) {
         req.body.parent = req.session.userId
         req.body.baby = babyId
@@ -29,6 +28,49 @@ router.post('/:babyId', (req, res) => {
         })
         .catch(err => res.redirect(`/error?error=${err}`))
 })
+
+
+// GET - Show edit page
+router.get('/:babyId/:sleepId/edit', (req, res) => {
+    const username = req.session.username
+	const loggedIn = req.session.loggedIn
+	const userId = req.session.userId
+    const babyId = req.params.babyId 
+    const sleepId = req.params.sleepId
+    Baby.findById(babyId)
+        .then(baby => {
+            const theSleep = baby.sleepSessions.id(sleepId)
+            res.render('sleep/edit', { baby, theSleep, username, loggedIn, userId })
+        })
+        .catch(error => {
+            res.redirect(`/error?error=${error}`)
+        })
+})
+
+// PUT - Edit subdoc
+router.put('/:babyId/:sleepId', (req, res) => {
+    const babyId = req.params.babyId 
+    const sleepId = req.params.sleepId
+    Baby.findById(babyId)
+        .then(baby => {
+            const theSleep = baby.sleepSessions.id(sleepId)
+            if (req.session.loggedIn) {
+                if (theSleep.parent == req.session.userId) {
+                    theSleep.set(req.body)
+                    baby.save()
+                    res.redirect(`/babies/${baby.id}`)
+                } else {
+                    const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+                }
+            } else {
+                const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+            }
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
+})
+
 
 // DELETE
 router.delete('/delete/:babyId/:sleepId', (req, res) => {
