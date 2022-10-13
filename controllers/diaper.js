16f013b6228cad86539399f056682f947aa729c4
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-13 12:58:54 -0400

    adds PUT and GET for edit subdoc

--- controllers/diaper.js
+++ controllers/diaper.js
@@ -30,6 +30,53 @@ router.post('/:babyId', (req, res) => {
         .catch(err => res.redirect(`/error?error=${err}`))
 })
 
+
+// GET - Show edit page
+router.get('/:babyId/:diaperId/edit', (req, res) => {
+    const username = req.session.username
+	const loggedIn = req.session.loggedIn
+	const userId = req.session.userId
+    const babyId = req.params.babyId 
+    const diaperId = req.params.diaperId
+    Baby.findById(babyId)
+        .then(baby => {
+            const theDiaper = baby.diapers.id(diaperId)
+            console.log('this is the diaper change that was found', theDiaper)
+            res.render('diapers/edit', { baby, theDiaper, username, loggedIn, userId })
+        })
+        .catch(error => {
+            res.redirect(`/error?error=${error}`)
+        })
+})
+
+// PUT - Edit subdoc
+router.put('/:babyId/:diaperId', (req, res) => {
+    const babyId = req.params.babyId 
+    const diaperId = req.params.diaperId
+    req.body.pee = req.body.pee === 'on' ? true : false
+    req.body.poop = req.body.poop === 'on' ? true : false
+    Baby.findById(babyId)
+        .then(baby => {
+            const theDiaper = baby.diapers.id(diaperId)
+            console.log('this is the diaper change that was found', theDiaper)
+            if (req.session.loggedIn) {
+                if (theDiaper.parent == req.session.userId) {
+                    theDiaper.set(req.body)
+                    baby.save()
+                    res.redirect(`/babies/${baby.id}`)
+                } else {
+                    const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+                }
+            } else {
+                const err = 'you%20are%20not%20authorized%20for%20this%20action'
+                    res.redirect(`/error?error=${err}`)
+            }
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
+})
+
+
 // DELETE
 router.delete('/delete/:babyId/:diaperId', (req, res) => {
     // isolate the ids and save to vars for easy ref
