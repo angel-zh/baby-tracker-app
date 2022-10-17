2fef59c3700767f9047a543ed3a4c19c0739d4fd
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-17 15:38:08 -0400

    updates seed documents

--- models/seed.js
+++ models/seed.js
@@ -7,10 +7,18 @@ const User = require('./user')
 const db = mongoose.connection
 db.on('open', () => {
     const startBabies = [
-        { firstName: 'Miles', lastName: 'Janson', dateOfBirth: '2022/08/10', gender: 'Boy', owner: '6345d2fee678fe23368ad3be' },
-        { firstName: 'Sally', lastName: 'Pickle', dateOfBirth: '2022/09/25', gender: 'Girl', owner: '6345d2fee678fe23368ad3be' },
-        // { firstName: 'Keith', lastName: 'Evanson', dateOfBirth: '2022-09-02', gender: 'Boy' },
-        // { firstName: 'Chloe', lastName: 'Evanson', dateOfBirth: '2022-09-02', gender: 'Girl' },
+        { firstName: 'Miles', lastName: 'Janson', dateOfBirth: '2022/08/10', gender: 'Boy', eye: 'Green', hair: 'Brown', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Sally', lastName: 'Pickle', dateOfBirth: '2022/08/13', gender: 'Girl', eye: 'Hazel', hair: 'Blond', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Fiona', lastName: 'Chan', dateOfBirth: '2022/07/25', gender: 'Girl', eye: 'Black', hair: 'Black', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Keith', lastName: 'Hutson', dateOfBirth: '2022/06/02', gender: 'Boy', eye: 'Blue', hair: 'Brown', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Chloe', lastName: 'Hutson', dateOfBirth: '2022/06/02', gender: 'Girl', eye: 'Blue', hair: 'Brown', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Mark', lastName: 'Peterson', dateOfBirth: '2022/08/20', gender: 'Boy', eye: 'Brown', hair: 'Brown', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Helen', lastName: 'Park', dateOfBirth: '2022/09/02', gender: 'Girl', eye: 'Black', hair: 'Brown', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Dwyane', lastName: 'Irvine', dateOfBirth: '2022/05/16', gender: 'Boy', eye: 'Hazel', hair: 'Blond', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Wilfred', lastName: 'Irvine', dateOfBirth: '2022/05/16', gender: 'Boy', eye: 'Blue', hair: 'Blond', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Marcus', lastName: 'Irvine', dateOfBirth: '2022/05/16', gender: 'Boy', eye: 'Blue', hair: 'Blond', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Gabrielle', lastName: 'Lee', dateOfBirth: '2022/06/19', gender: 'Girl', eye: 'Black', hair: 'Black', owner: '634dad18c9cb6254c1555128' },
+        { firstName: 'Zoey', lastName: 'Jung', dateOfBirth: '2022/07/10', gender: 'Girl', eye: 'Black', hair: 'Black', owner: '634dad18c9cb6254c1555128' },
     ]
 
     Baby.deleteMany ()
